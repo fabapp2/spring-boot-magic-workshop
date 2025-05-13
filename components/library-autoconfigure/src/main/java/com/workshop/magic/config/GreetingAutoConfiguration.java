c469b2a9407ce1c1255d572dbfc5a31348b4a4e5
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-05-13 14:55:00 +0200

    Exercise 4

--- components/library-autoconfigure/src/main/java/com/workshop/magic/config/GreetingAutoConfiguration.java
+++ components/library-autoconfigure/src/main/java/com/workshop/magic/config/GreetingAutoConfiguration.java
@@ -1,11 +1,13 @@
 package com.workshop.magic.config;
 
 import com.workshop.magic.service.GreetingService;
+import com.workshop.magic.service.slf4j.LoggerGreetingService;
 import com.workshop.magic.service.stdout.StdOutGreetingService;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
@@ -15,8 +17,16 @@ public class GreetingAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnClass(StdOutGreetingService.class)
+    @ConditionalOnMissingClass("com.workshop.magic.service.slf4j.LoggerGreetingService")
     GreetingService stdOutGreetingService() {
         return new StdOutGreetingService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnClass(LoggerGreetingService.class)
+    GreetingService slf4jGreetingService() {
+        return new LoggerGreetingService();
     }
 
 }
